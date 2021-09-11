b4c9600b7a928211f34e3b944e6d1d23c40b54aa
renaisun <43090234+renaisun@users.noreply.github.com>
2021-09-11 23:59:04 +0800

    test progressbar indicator

--- app/src/main/java/com/my/bubbletea/user/LoginActivity.java
+++ app/src/main/java/com/my/bubbletea/user/LoginActivity.java
@@ -2,9 +2,35 @@ package com.my.bubbletea.user;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.progressindicator.LinearProgressIndicator;
 import com.my.bubbletea.R;
+
+class TimeConsumer implements Runnable {
+    private Thread t;
+    private String threadName;
+
+    TimeConsumer(String threadName) {
+        this.threadName = threadName;
+    }
+
+    public void run() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
+        if (t== null) {
+            t = new Thread(this, threadName);
+            t.start();
+        }
+    }
+}
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -19,7 +45,17 @@ public class LoginActivity extends AppCompatActivity {
         submitLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // prevent users from clicking again.
                 submitLogin.setEnabled(false);
+
+                LinearProgressIndicator lpi = findViewById(R.id.login_progress_bar);
+                lpi.show();
+                Toast.makeText(getApplicationContext(),"Successfully logged in.",Toast.LENGTH_SHORT).show();
+                TimeConsumer tc = new TimeConsumer("Hi");
+                tc.run();
+//                lpi.hide();
+
+//                Toast.makeText(getApplicationContext(),"Successfully logged in.",Toast.LENGTH_LONG).show();
             }
         });
     }
